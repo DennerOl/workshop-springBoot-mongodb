1cbca843a4ac1f1aa114aae10b32e15a10602cc5
DennerOl <dener155@hotmail.com>
2023-05-30 21:35:19 -0300

    DTO para retornar Usuarios

--- src/main/java/com/denerol/workshopmongo/resources/UserResource.java
+++ src/main/java/com/denerol/workshopmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.denerol.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.denerol.workshopmongo.domain.User;
+import com.denerol.workshopmongo.dto.UserDTO;
 import com.denerol.workshopmongo.services.UserService;
 
 @RestController
@@ -22,8 +22,11 @@ public class UserResource {
 
 // retorna uma lista de usuarios do Service
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+// converto a lista do tipo user para userDTO
+		List<UserDTO> listDto = 
+		list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
